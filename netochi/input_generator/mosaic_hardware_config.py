efb08661694c48437c31ad258411b677d0b34654
laura erb <laura.erb1@gmx.de>
2026-05-21 13:42:41 +0200

    router levels can be zero (only one core)

--- netochi/input_generator/mosaic_hardware_config.py
+++ netochi/input_generator/mosaic_hardware_config.py
@@ -7,7 +7,7 @@ class MosaicHardwareConfig(BaseModel):
 
     nodes_per_router: int = Field(gt=0, description="Number of nodes connected to each router.")
     neurons_per_core: int = Field(gt=0, description="Number of neurons in each core.")
-    router_levels: int = Field(gt=0, description="Number of levels in the router hierarchy.")
+    router_levels: int = Field(ge=0, description="Number of levels in the router hierarchy.")
     slice_factor: int = Field(default=2, gt=0, description="Factor determining slice sizes for fan-in.")
 
     @property
