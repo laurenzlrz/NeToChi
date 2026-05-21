e79347044da53d0f6346513457734c0d5a0f28db
laura erb <laura.erb1@gmx.de>
2026-05-21 10:17:18 +0200

    add hcd hw clusterer

---
+++ netochi/mapping/three_step_mapping/clustering/hcd_hw_clusterer.py
@@ -0,0 +1,19 @@
+from netochi.input_generator.interfaces import MappingInput
+from netochi.mapping.three_step_mapping.clustering.cluster_adapter.padding_adapter import PaddingClusteringAdapter
+from netochi.mapping.three_step_mapping.clustering.hierarchical_community_detection.hcd_clusterer import HcdClusterer
+from netochi.mapping.three_step_mapping.clustering.hw_clusterer_adapter import HwClustererAdapter
+from netochi.mapping.three_step_mapping.interfaces import HwClusterer, ClusterAndHwOutput
+
+
+class HcdHwClusterer(HwClusterer):
+    """
+    1. infers clustering using the Hierarchical Community Detection
+    2. infers hardware and fits clustering to it using the padding adapter
+    """
+
+    def __init__(self):
+        self._clusterer = HwClustererAdapter(clusterer=HcdClusterer(), adapter=PaddingClusteringAdapter())
+
+    def cluster(self, input_data: MappingInput) -> ClusterAndHwOutput:
+        return self._clusterer.cluster(input_data)
+
