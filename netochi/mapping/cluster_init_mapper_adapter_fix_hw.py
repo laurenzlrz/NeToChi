fa5ab746ac251e8536bb5bbbca9a916319733ddf
laure <laurenz.wurzel@gmail.com>
2026-06-12 18:04:49 +0200

    Fixes cluster init mapper adapter

--- netochi/mapping/cluster_init_mapper_adapter_fix_hw.py
+++ netochi/mapping/cluster_init_mapper_adapter_fix_hw.py
@@ -2,24 +2,32 @@
 from typing import Any
 
 from netochi.mapping.interfaces import BaseMapper, MosaicNetworkMappingState
-from netochi.input_generator.interfaces import MosaicHWMappingInput
+from netochi.input_generator.interfaces import MosaicMappingInput
 
 
 from netochi.mapping.three_step_mapping.interfaces import ClustererFixedHw
 
 
-class HwClusterInitMapper(BaseMapper[MosaicNetworkMappingState[Any], MosaicHWMappingInput[Any]]):
+class HwClusterInitMapper(BaseMapper[MosaicNetworkMappingState, MosaicMappingInput]):
     """
     Given a HwClusterer and a Mapper, it computes the clustering and passes the clustering to the mapper as initialization
     """
 
-    def __init__(self, clusterer: ClustererFixedHw, mapper: BaseMapper[MosaicNetworkMappingState, MosaicHWMappingInput]):
+    def __init__(self, clusterer: ClustererFixedHw, mapper: BaseMapper[MosaicNetworkMappingState, MosaicMappingInput]):
         self.clusterer = clusterer
         self.mapper = mapper
 
-    def run(self, mapping_input: MosaicHWMappingInput) -> MosaicNetworkMappingState[Any]:
+    def run(self, mapping_input: MosaicMappingInput) -> MosaicNetworkMappingState[Any]:
         # --- 1. compute cluster initialization ---
+        #TODO: IMPROVE ERROR VALIDATION
         clustering = self.clusterer.cluster(mapping_input)
-        mapping_input.core_assignment_initialization = clustering
+        if (clustering.hw != mapping_input.hw_config):
+            raise ValueError(f"Clusterer returned a different hardware configuration than the input. Clusterer hw: {clustering.hw}, input hw: {mapping_input.hw_config}")
+
+        # TODO: IMPROVE INITIALIZATION.
+        mapping_input = MosaicMappingInput(
+            graph=mapping_input.graph,
+            hw_config=clustering.hw,
+            core_assignment_initialization=clustering)
         return self.mapper.run(mapping_input)
 
--- netochi/mapping/cluster_init_mapper_adapter_infer_hw.py
+++ netochi/mapping/cluster_init_mapper_adapter_infer_hw.py
@@ -7,7 +7,7 @@ from netochi.input_generator.interfaces import MappingInput
 from netochi.mapping.three_step_mapping.interfaces import ClustererInferHw
 
 
-class ClusterInitMapper(BaseMapper[MosaicHWMappingState[Any], MappingInput[Any]]):
+class ClusterInitMapper(BaseMapper[MosaicHWMappingState, MappingInput]):
     """
     Given a Clusterer and a mapper, HwClusterer and a Mapper, it computes the clustering and passes the clustering to the mapper as initialization
     """
@@ -19,6 +19,9 @@ class ClusterInitMapper(BaseMapper[MosaicHWMappingState[Any], MappingInput[Any]]
     def run(self, mapping_input: MappingInput) -> MosaicHWMappingState:
         # --- 1. compute cluster initialization ---
         clustering = self.clusterer.cluster(mapping_input)
-        mapping_input.core_assignment_initialization = clustering
+        mapping_input = MosaicMappingInput(
+            graph=mapping_input.graph,
+            hw_config=clustering.hw,
+            core_assignment_initialization=clustering)
         return self.mapper.run(mapping_input)
 
