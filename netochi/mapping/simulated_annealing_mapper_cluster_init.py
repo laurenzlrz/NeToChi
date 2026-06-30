fa7122f456f160f5b43cf761dfa74bbeb41afac8
laura erb <laura.erb1@gmx.de>
2026-06-30 09:06:55 +0200

    add three step mapper configs

--- netochi/mapping/simulated_annealing_mapper_cluster_init.py
+++ netochi/mapping/simulated_annealing_mapper_cluster_init.py
@@ -1,6 +1,16 @@
+import icontract
+from pydantic import BaseModel, Field
+
 from netochi.mapping.cluster_init_mapper_adapter_fix_hw import HwClusterInitMapper
-from netochi.mapping.simulated_annealing_mapper import SimAnnealingMapper
+from netochi.mapping.simulated_annealing_mapper import SimAnnealingMapper, SimAnnealingMapperConfig
 from netochi.mapping.three_step_mapping.clustering.hsbm_hw_clusterer import HsbmHwClusterer
+
+
+class SimulatedAnnealingMapperClusterInitConfig(BaseModel):
+    verbose: bool = Field(default=False)
+
+    def create(self) -> "SimulatedAnnealingMapperClusterInit":
+        return SimulatedAnnealingMapperClusterInit(config=self)
 
 
 class SimulatedAnnealingMapperClusterInit(HwClusterInitMapper):
@@ -8,9 +18,10 @@ class SimulatedAnnealingMapperClusterInit(HwClusterInitMapper):
     Given a HwClusterer and a Mapper, it computes a clustering that fits on the hardware and passes the clustering to the mapper as initialization
     """
 
-    def __init__(self):
+    @icontract.require(lambda config: isinstance(config, SimulatedAnnealingMapperClusterInitConfig))
+    def __init__(self, config: SimulatedAnnealingMapperClusterInitConfig):
         clusterer = HsbmHwClusterer()
-        mapper = SimAnnealingMapper()
+        mapper = SimAnnealingMapperConfig().create()
         super().__init__(clusterer, mapper)
 
 
